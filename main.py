051e9a94f9b0816c36256d6f1c9652f66dcf4ab8
bosscharlie <67408162+bosscharlie@users.noreply.github.com>
2021-01-12 17:32:30 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from player import player
+myplayer=player('COM6','COM7','COM5',90,1)  #par 串口地址（左手、右手、控制阀）、BPM、id标号
+myplayer.init()
+
+myplayer.play_sound('C',1)  #参数为音名及时长
+myplayer.play_sound('D',1)
+myplayer.play_sound('E',1)
+myplayer.play_sound('F',1)
+myplayer.play_sound('G',1)
+myplayer.play_sound('A',1)
+myplayer.play_sound('B',1)
+myplayer.play_sound('HC',1)
+myplayer.play_sound('HD',1)
+myplayer.init()
