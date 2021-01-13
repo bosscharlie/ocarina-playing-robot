a4acceda1c33261271571820642b09dc7ab52c66
Charlie <878574650@qq.com>
2021-01-13 16:46:50 +0800

    get audio from video

--- fundamental-frequency.py
+++ fundamental-frequency.py
@@ -4,3 +4,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import wave
 import soundfile as sf
+import os
+import glob
+from moviepy.editor import *
+from pydub import AudioSegment
+
+video = VideoFileClip('test.mp4')
+audio = video.audio
+audio.write_audiofile('test.mp3')
