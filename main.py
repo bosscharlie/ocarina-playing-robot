e4772129c68641fd17e8dcff49e0e204d0afc5b8
卫俊锋 <2018050029@secoder.net>
2021-01-25 08:47:15 +0800

    气流控制与手势控制联合调通

--- main.py
+++ main.py
@@ -2,51 +2,44 @@ from player import player
 import fundamental
 import _thread
 import time
-myplayer=player('COM4','COM5','COM3',90,1)  #par 串口地址（左手、右手、控制阀）、BPM、id标号
-myplayer.init()
-fundamental.preprocessing()
-result=fundamental.sampling()
-def air_control():
-    f = open("littleStar_air.txt","r")
-    line = f.readline()
-    while(line):
-        print(line)
-        a = line.split(" ")
-        #time.sleep(float(a[1]))
-        myplayer.choose_power(int(a[0]),float(a[1]))
-        time.sleep(float(a[1]))
-        line = f.readline()
+from removeNoise import start,stand,intensity
+
+myplayer=player('COM3','COM9','COM4','COM10',90,1)  #par 串口地址（左手、右手、控制阀）、BPM、id标号
+Sampling_interval_time = 0.1    #表示力度采样的时间间隔
+standard_value_num = 700
+#myplayer.init()
+#fundamental.preprocessing()
+#result=fundamental.sampling()
+start()
+
+def air_control(a):
+    for i in range(len(a)):
+        if(int(a[i]) == 0):
+            max = 1850 - int(a[i])
+            myplayer.set_separate(False)
+            myplayer.choose_power(max, (Sampling_interval_time) / 2)
+            time.sleep(Sampling_interval_time/ 2)
+        else:
+            myplayer.set_separate(True)
+            myplayer.choose_power(1850 - int(a[i]),(Sampling_interval_time)/2)
+            time.sleep(Sampling_interval_time/2)
 
 def play_control():
     a=open('result.txt')
     result=[]
     for line in a.readline():
         result.append(line)
-    print('end')
     for i in range(0,len(result)):
         if(result[i]=='N'):
             continue
         myplayer.play_sound(result[i],60/(fundamental.bpm*4))
 
 if __name__ == '__main__':
-    for i in range(1000):
-        myplayer.set_separate(True)
-        print("true")
-        time.sleep(1)
-        myplayer.set_separate(False)
-        print("false")
-        time.sleep(1)
-    myplayer.set_separate(True)
-    print("true")
-    time.sleep(0.01)
-    myplayer.set_separate(False)
-    print("false")
-    time.sleep(0.01)
     # 创建两个线程
     try:
+        print("nmh")
         _thread.start_new_thread(play_control, ())
-        myplayer.set_separate(True)
-        _thread.start_new_thread(air_control, ())
+        _thread.start_new_thread(air_control, (intensity,))
     except:
         print("Error: 无法启动线程")
 
